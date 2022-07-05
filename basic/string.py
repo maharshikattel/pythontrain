f24f293a821cf73ecc2b77dea1830caa77cbfdfe
= <=>
2022-07-05 17:40:22 +0545

    :tada: Calculator and cantenation

---
+++ basic/string.py
@@ -0,0 +1,31 @@
+"""
+print(9**4) # power of 
+print(9*4) # multiplication
+print(9/4) # float division
+print(9//4) # integer divide
+print(9+4) # addition
+print(9-4) # subtraction
+print(9%4) # modulo
+"""
+print(round(2**3.14,3)) # only displays 3 first digits of decimal
+"""m
+u
+l
+t
+i 
+
+LINE COMMENT EXAMPLE
+"""
+
+print ("\U0001F972") # replace + by three 0's
+
+ # python can do simplification too
+
+
+print ("Samay"+" "+"Chaudhary") # String concantenation
+
+# to join string and integer:
+
+print("Aryama"+str(10))
+
+
