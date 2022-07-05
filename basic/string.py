a2fe59c1df0d047fcce3b852808903fcf00371d1
= <=>
2022-07-05 17:55:15 +0545

    :wrench:Added info

--- basic/string.py
+++ basic/string.py
@@ -7,7 +7,7 @@ print(9+4) # addition
 print(9-4) # subtraction
 print(9%4) # modulo
 """
-print(round(2**3.14,3)) # only displays 3 first digits of decimal
+# print(round(2**3.14,3)) # only displays 3 first digits of decimal
 """m
 u
 l
@@ -17,15 +17,36 @@ i
 LINE COMMENT EXAMPLE
 """
 
-print ("\U0001F972") # replace + by three 0's
+# print ("\U0001F972") # replace + by three 0's
 
  # python can do simplification too
 
 
-print ("Samay"+" "+"Chaudhary") # String concantenation
+# print ("Samay"+" "+"Chaudhary") # String concantenation
 
 # to join string and integer:
 
-print("Aryama"+str(10))
+# print("Aryama"+str(10))
+
+
+# Variables in python :
+
+"""
+a = 11
+print("The first value of a -->", a)
+a = "Apple"
+print("The second value of a -->", a)
+"""
+numericvalue = 12 
+print("the first value of a is -->",numericvalue)
+fruitname = "Apple"
+print("the second vaue of a is -->",fruitname)
+
+"""
+numeric_value ==> snake_case_writing (recommended for python)
+numericValue ==> camelCaseWriting
+"""
+
+
 
 
