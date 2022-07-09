a0d04e7461cd15f130fcd48c7a8fafb09b6fb777
unknown <mercyyt322@example.com>
2022-07-09 08:00:57 +0545

    :wrench: still working

--- string02.py
+++ string02.py
@@ -5,8 +5,56 @@ first_name, second_name = input("Provide two fruits (comma separated): ").split(
 """
 
 #Type function
-
+"""
 type()
 name = input ("Give name: ")
-type(name)
+type(name)"""
+
+#Average
+"""a,b,c = input("Enter three numbers(comma separated): ").split(",")
+s = int(a) + int(b) + int(c)
+print(s//3)
+"""
+#String indexing
+"""name = "language"
+print(name) #language
+print(name[3]) #g
+print(name[-1]) #e
+print(name[3:8]) #guage
+print(name[1:7:2]) #age (the last '2' is the skip number)
+
+print(len(name)) #8
+print(name[::-1]) #prints reverse
+print(name[-1::-1]) #prints reverse from the starting point -1"""
+
+#String method
+"""example = "LeArN PyThOn"
+print(example.lower()) # learn python
+print(example.upper()) # LEARN PYTHON
+print(example.title()) # Learn Python
+print(example.count("p")) #counts number of small p's ----its case sensitive"""
+
+#Asking name & counting a character
+"""frst_name, secnd_name = input("Enter name (space separated): ").split(" ")
+full_name = frst_name + secnd_name
+print(full_name.count("a"))"""
+
+#String methods can be used in the same line together
+print("example.lower().count("a")")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
