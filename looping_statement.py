2c10da9aedbf36406559b2863199872e7a0eb132
unknown <mercyyt322@example.com>
2022-07-09 06:14:45 +0545

    :hammer: Thor's day

---
+++ looping_statement.py
@@ -0,0 +1,58 @@
+"""i = 1
+while i<=10:
+    print(f"Hello World {i}")
+    i += 1"""
+
+"""for i in range(0,10,2):  #Last ko 2 adds the interval
+    print(f"for loop {i}")
+"""
+
+
+
+"""number = int(input("Enter the final number: "))
+i = 1
+int(i)
+sum = 0
+int(sum)
+while i<=number:
+    sum += i
+    i += 1
+    
+print("Sum is " + str(sum))"""
+
+"""
+number = input("Enter a four digit number: ")
+i = len(number)
+int(number)
+sum = 0
+int(sum)
+digit = 0
+int(digit)
+while i > 0:
+    digit = number[i]
+    sum += digit
+    number = number%10
+print("Sum is " + str(sum))"""
+
+full_name = input("Enter a full name: ")
+store = ""
+i = 0
+while i < len(full_name):
+    if full_name[i] not in store:
+        print(f"{full_name[i]} : {full_name.upper().count(full_name[i].upper())}")
+        i += 1
+
+
+# break and continue keyword
+"""for i in range(1,10):
+    if i==5:
+        continue # continues without the ongoing loop
+
+    print(f"The new number is: {i}")
+
+for i in range(1,10):
+    if i==5:
+        break # ends the code
+
+    print(f"The new number is: {i}")
+"""
